5ccdefbd6268ec8ab2fcaf32e20ac494454a53c4
tabish615 <tabish615@gmail.com>
2020-02-08 21:11:25 +0500

    class_code_08-02-2020

---
+++ class_code_08-02-2020/script.js
@@ -0,0 +1,32 @@
+
+// var total = sum(5, 2)
+// console.log(total)
+
+// function sum(a, b) {
+//     return a + b
+// }
+
+// console.log(a)
+
+// var a = 2
+
+// console.log(a)
+
+
+// var total = sum(5, 2)
+// console.log(total)
+
+// var sum = function (a, b) {
+//     return a + b
+// }
+
+
+// function greet(message){
+//     alert(message)
+// }
+
+function greet(){
+    var message = prompt("enter your name")
+    alert(message)
+}
+
