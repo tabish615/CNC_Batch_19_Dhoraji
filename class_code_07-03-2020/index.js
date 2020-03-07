ea56de68a23f9b0c26b4ea4867378b9459f0c582
tabish615 <tabish615@gmail.com>
2020-03-07 21:15:59 +0500

    class_code_07-03-2020

---
+++ class_code_07-03-2020/index.js
@@ -0,0 +1,34 @@
+// var signUpWindow;
+// var signInWindow;
+
+
+// function signUpForm () {
+//     signUpWindow = window.open('signUp.html', '', 'width=420,height=380,left=200,top=100')   
+// }
+// function signInForm () {
+//     signInWindow = window.open('signIn.html', '', 'width=420,height=380,left=200,top=100')   
+// }
+// function UserData (name, email, password) {
+//     this.name = name,
+//     this.email = email,
+//     this.password = password
+// }
+
+// function signUpSubmit () {
+//     var userName = document.getElementById('username').value
+//     var userEmail = document.getElementById('useremail').value
+//     var userPassword = document.getElementById('userpassword').value
+//     var user = new UserData(userName, userEmail, userPassword)
+//     localStorage.setItem(`${userEmail}`, JSON.stringify(user))
+//     signUpWindow = window.close();
+// }
+
+// function signInSubmit () {
+//     var userEmail = document.getElementById('useremail').value
+//     var userPassword = document.getElementById('userpassword').value
+//     // var user = localStorage.getItem(userEmail)
+//     // console.log("USER",user)
+//     // if(user){
+//     // }
+//     signInWindow = window.close();
+// }
