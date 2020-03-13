3a6be689640eb6a8cbbeea6db527f6b5bdfaf344
tabish615 <tabish615@gmail.com>
2020-03-13 21:08:34 +0500

    class_code_13-03-2020

---
+++ Advanced JS/class_code_13-03-2020/index.js
@@ -0,0 +1,141 @@
+// try {
+//     aler("hello")
+// } catch (error) {
+//     alert(error)
+// }
+
+// try {
+//     if (true) {
+//         throw "PLEASE CHECK ALERT"
+//     }
+// } catch (error) {
+//     alert(error)
+// }
+
+// ES6 Let and const
+
+// {
+//     // var name = "Akmal"
+//     let x = 6
+// }
+
+// console.log(name)
+
+// console.log(x)
+
+// let x = 5
+// {
+//     let x = 2
+//     {
+//          const x = 4
+//         console.log(x)
+
+//     }
+//     console.log(x)
+// }
+
+// console.log(x)
+
+// let x = 5
+// {
+//     const x = 2
+//     {
+//         // x = 4
+//         console.log(x)
+
+//     }
+//     console.log(x)
+// }
+
+// console.log(x)
+
+// {
+//     const arr =  ["abc"]
+//     {
+//         const arr = ["def"]
+//         console.log(arr)
+//     }
+// }
+
+
+
+// {
+//     const arr =  ["abc"]
+//     {
+//         arr = ["def"]
+//         console.log(arr)
+//     }
+// }
+
+
+// {
+//     const arr =  ["abc"]
+//     {
+//         arr[0] = "def"
+//         console.log(arr)
+//     }
+// }
+
+
+// {
+//     const arr =  {"name":"Akmal"}
+//     {
+//         arr["name"] = "Ali"
+//         console.log(arr)
+//     }
+// }
+
+
+// setTimeOut and setInterval
+
+// setTimeout(function(){
+//     //  console.log("HELLO")
+//     alert("HELLO")
+// },5000)
+
+
+// setInterval(function(){
+//      console.log("HELLO")
+// },2000);
+
+// clearInterval()
+
+
+
+// console.log(1)
+
+// setTimeout(function(){
+//      console.log(2)
+// },2000)
+
+// console.log(3)
+
+
+// console.log(1)
+
+// setTimeout(function(){
+//      console.log(2)
+// },0)
+
+// console.log(3)
+
+
+// setTimeout(function(){
+
+//      console.log(1)
+
+//      setTimeout(function() {
+
+//         setTimeout(function() {
+
+//             console.log(3)
+//          }, 2000);
+
+//         console.log(2)
+
+//      }, 3000);
+
+//      console.log(4)
+// },5000)
+
+
